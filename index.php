f463886f10b0361f1be0327ef2f6080c5a755320
Daniel Supernault <danielsupernault@gmail.com>
2012-04-26 21:52:32 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Project Meshnet</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="github.com/dansup">
+
+    <!-- Le styles -->
+    <link href="./css/bootstrap.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
+      }
+    </style>
+    <link href="./css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+   
+
+    <div class="container">
+	   <div class="row">
+        <div class="span8">
+		<img src="./images/lead.png" />
+     </div>
+	  <div class="span4">
+		<ul class="nav nav-pills">
+  <li class="active">
+    <a href="./">Home</a>
+  </li>
+  <li><a href="http://wiki.projectmeshnet.org/Getting_started">get started</a></li>
+  <li><a href="http://hyperboria.net">network</a></li>
+  <li><a href="http://forum.projectmeshnet.org">forum</a></li>
+  <li><a href="https://wiki.projectmeshnet.org">wiki</a></li>
+</ul>
+     </div></div><br>
+	  <div class="hero-unit">
+        <h1>internet evolved</h1>
+        <p>established to create a sustainable, decentralized and scalable global mesh network.</p>
+      </div>
+
+     
+      <hr>
+	</div> 
+		<script type="text/javascript">
+		var pkBaseURL = (("https:" == document.location.protocol) ? "https://projectmeshnet.org/ai/" : "http://projectmeshnet.org/ai/");
+		document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+		</script><script type="text/javascript">
+		try {
+		var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 1);
+		piwikTracker.trackPageView();
+		piwikTracker.enableLinkTracking();
+		} catch( err ) {}
+        </script><noscript><p><img src="https://projectmeshnet.org/ai/piwik.php?idsite=1" style="border:0" alt="" /></p></noscript>
+	 <script src="./js/jquery.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+    <script src="./js/dropdown.js"></script>
+  </body>
+</html>
