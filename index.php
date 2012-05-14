b6f382e83ff75414ab60501a66088f4369314551
Daniel Supernault <danielsupernault@gmail.com>
2012-05-13 23:33:43 -0600

    added more content

--- index.php
+++ index.php
@@ -29,10 +29,10 @@
 
     <div class="container">
 	   <div class="row">
-        <div class="span8">
+        <div class="span6">
 		<img src="./images/lead.png" />
      </div>
-	  <div class="span4">
+	  <div class="span6">
 		<ul class="nav nav-pills">
   <li class="active">
     <a href="./">Home</a>
@@ -42,16 +42,96 @@
   <li><a href="http://forum.projectmeshnet.org">forum</a></li>
   <li><a href="https://wiki.projectmeshnet.org">wiki</a></li>
 </ul>
-     </div></div><br>
+     </div></div><br><div class='container'>
 	  <div class="hero-unit">
-        <h1>internet evolved</h1>
-        <p>established to create a sustainable, decentralized and scalable global mesh network.</p>
-      </div>
+        <h3>our objective is to create a versatile, decentralized network built on secure protocols for routing traffic over private mesh or public internetworks independent of a central supporting infrastructure.</h3>
 
+      </div> 
+<div class="row pagination-centered">
+			<div class="span4">
+				<div class="well">
+					<h1>Get Started</h1>
+					<br />
+					<img src="./images/getstarted.png">
+					<br /><br />
+					<p>Create a node and join the network.</p>
+					<p><a class="btn btn-large btn-primary" href="https://wiki.projectmeshnet.org/Getting_Started">Getting Started Guide</a></p>
+				</div>
+			</div>
+			<div class="span4">
+				<div class="well">
+					<h1>Docs</h1>
+					<br />
+					<img src="./images/docs.png">
+					<br /><br />
+					<p>CJDNS Technical Whitepaper.</p>
+					<p><a class="btn btn-large btn-primary" href="https://github.com/cjdelisle/cjdns/blob/master/rfcs/Whitepaper.md">Technical Documentation</a></p>
+				</div>
+			</div>
+			<div class="span4">
+				<div class="well">
+					<h1>Discuss</h1>
+					<br /><br />
+					<img src="./images/chat.png">
+					<br /><br />
+					<p>Chat with the developers of the project.</p> 
+					<p><a class="btn btn-large btn-primary" href="http://chat.efnet.org:9090/?channels=%23cjdns&Login=Login">EFnet Webchat</a></p>
+				</div>
+			</div></div><hr>
+  <div class='row'><div class="page-header">
+    <h1>Current Projects <small> Project Meshnet collaborates with several <a href="http://en.wikipedia.org/wiki/Free_and_open_source_software">FOSS</a> mesh related projects </small></h1>
+  </div><div class='span8'>
+          <h1 class='lead'>
+            <strong>CJDNS</strong> - a routing engine designed for security, scalability, speed and ease of use.
+        </div>
+		 <div class='span3 offset1'>
+          <p>
+<a class='btn btn-large btn-primary' href='https://github.com/cjdelisle/cjdns/' >
+             CJDNS Github</a>
+          </p>
+            <small>
+              official cjdns repo
+            </small>
+        </div>
+      </div>
+      <hr />    
+	<div class='row'>
+        <div class='span8'>
+          <h1 class='lead'>
+            <strong>Hyperboria</strong> - a decentralized network of <a href="http://cjdns.info">cjdns</a> nodes established with the prinicples of freedom and security at its core.
+        </div>
+        <div class='span2'>
+          <p>
+              <strong>
+               
+              </strong><a class='btn btn-large btn-success' href='https://wiki.projectmeshnet.org/Known_Hyperboria_sites' >
+             Sites/Services</a>
+          </p>
+        
+            <small>
+             Hyperboria-only sites/services
+            </small>
+         
+        </div>
+		 <div class='span2'>
+          <p>
+              <strong>
+               
+              </strong><a class='btn btn-large btn-info' href='https://www.thefinn93.com/cjdns/map/' >
+             Hyperboria Map</a>
+          </p>
+        
+            <small>
+              a live look at the network
+            </small>
+         
+        </div>
+      </div>
+      <hr />
      
-      <hr>
+      
 	</div> 
-		<script type="text/javascript">
+			<script type="text/javascript">
 		var pkBaseURL = (("https:" == document.location.protocol) ? "https://projectmeshnet.org/ai/" : "http://projectmeshnet.org/ai/");
 		document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
 		</script><script type="text/javascript">
